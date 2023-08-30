288ef857894010a2e68c3e4424aea346be382fe8
HarisWei <haris@koinworks.com>
2023-08-30 22:51:26 +0700

    testing number formatter

--- app/src/main/java/com/hwx/interestcalculatorapp/CalculatorScreen.kt
+++ app/src/main/java/com/hwx/interestcalculatorapp/CalculatorScreen.kt
@@ -20,12 +20,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hwx.interestcalculatorapp.ui.theme.InterestCalculatorAppTheme
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -152,7 +160,9 @@ fun InputField(
     }
     OutlinedTextField(
         modifier = modifier,
-        value = data,
+        value = try {
+            data.toDouble().toFormat()
+        }catch (e:Exception) {""},
         onValueChange = { onValueChange ->
             try {
                 data = onValueChange
@@ -165,8 +175,58 @@ fun InputField(
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Number
         ),
+        visualTransformation = CurrencyVisualTransformation(),
         singleLine = true
     )
+}
+
+fun Double.toFormat(): String {
+    val nf = NumberFormat.getNumberInstance(Locale.ENGLISH)
+    val formatter = nf as DecimalFormat
+    formatter.applyPattern("#,###,###,###,###.###")
+    return "Rp " + formatter.format(this)
+}
+
+class CurrencyVisualTransformation() : VisualTransformation {
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        val trimmed = if (text.text.length >= 16) text.text.substring(0..15) else text.text
+        var out = ""
+        for (i in trimmed.indices) {
+            out += trimmed[i]
+            if (i % 4 == 3 && i != 15) out += "-"
+        }
+
+        /**
+         * The offset translator should ignore the hyphen characters, so conversion from
+         *  original offset to transformed text works like
+         *  - The 4th char of the original text is 5th char in the transformed text.
+         *  - The 13th char of the original text is 15th char in the transformed text.
+         *  Similarly, the reverse conversion works like
+         *  - The 5th char of the transformed text is 4th char in the original text.
+         *  - The 12th char of the transformed text is 10th char in the original text.
+         */
+        val creditCardOffsetTranslator = object : OffsetMapping {
+            override fun originalToTransformed(offset: Int): Int {
+                if (offset <= 3) return offset
+                if (offset <= 7) return offset + 1
+                if (offset <= 11) return offset + 2
+                if (offset <= 16) return offset + 3
+                return 19
+            }
+
+            override fun transformedToOriginal(offset: Int): Int {
+                if (offset <= 4) return offset
+                if (offset <= 9) return offset - 1
+                if (offset <= 14) return offset - 2
+                if (offset <= 19) return offset - 3
+                return 16
+            }
+        }
+
+        return TransformedText(AnnotatedString(out), creditCardOffsetTranslator)
+
+    }
 }
 
 @Preview(showBackground = true)
